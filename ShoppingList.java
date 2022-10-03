dbbd9448bde276fb7089a15cab879c359b6cc351
matej <matej.basta@email.cz>
2022-10-03 20:48:33 +0200

    comments added

--- ShoppingList.java
+++ ShoppingList.java
@@ -12,14 +12,18 @@ public class ShoppingList {
         createShoppingList();
     }
 
+    // function that creates the shopping list
     private void createShoppingList() {
+        // accessing both lists from InputReader
         Map<String, Ingredient> availableThings = inputReader.getThingsAtHome();
         Map<String, Ingredient> neededThings = inputReader.getThingsNeeded();
 
+        //looping through every item in list of needed things
         for (String key : neededThings.keySet()) {
             
             Ingredient neededIngredient = neededThings.get(key);
 
+            //putting the correct amount of needed ingredients into the shopping list
             if (availableThings.containsKey(key)) {
                 Ingredient availableIngredient = availableThings.get(key);
                 if (availableIngredient.getAmount() < neededIngredient.getAmount()) {
@@ -32,6 +36,7 @@ public class ShoppingList {
         }
     }
 
+    //function that prints the shopping list
     public void printShoppingList() {
         
         System.out.println("Shopping List:");
